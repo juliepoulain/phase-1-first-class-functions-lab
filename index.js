0aa493ff77153085603dbbab1d843f662f4d0979
Julie Poulain <juliempoulain@gmail.com>
2024-03-12 12:29:43 -0700

    created functions for lab

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Code your solution in this file!
+
+const returnFirstTwoDrivers = function (drivers) {
+  return [drivers[0], drivers[1]];
+};
+
+const returnLastTwoDrivers = function (drivers) {
+  return [drivers[2], drivers[3]];
+};
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(integer) {
+  const fareCalc = (fare) => {
+    return fare * integer;
+  };
+  return fareCalc;
+}
+
+const fareDoubler = createFareMultiplier(2);
+
+const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, returnFunction) {
+  return returnFunction(drivers)
+};
